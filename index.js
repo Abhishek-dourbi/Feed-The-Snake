eab7b17d2183877354267ef1ec1135bbfe6858b9
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2022-06-26 00:01:49 +0530

    add curved snake head

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// dom elements
 const grid = document.querySelector('.grid');
+
 const gridSize = 25;
 let currentIndexArr = [Math.pow(gridSize, 2) - Math.floor(gridSize * 1.5) - 1];
 let interval = 1;
 let snakeIntervalId;
+let currentSnakeHeadDirection = 'right';
+let snakeHeadStyles = [
+    'snake-head-top',
+    'snake-head-right',
+    'snake-head-bottom',
+    'snake-head-left',
+]
 
 for(let i = 0; i < Math.pow(gridSize, 2); i++) {
     const square = document.createElement('div');
@@ -27,6 +36,7 @@ function checkForFood() {
     const snakeHeadElement = squares[currentIndexArr[currentIndexArr.length - 1]];
     if(snakeHeadElement.classList.contains('food')) {
         snakeHeadElement.classList.remove('food');
+        removeSnakeHeadStyle();
         currentIndexArr.push(currentIndexArr[currentIndexArr.length - 1] + interval);
         addSnakeHead();
         drawFood();
@@ -43,7 +53,16 @@ function checkForTangle() {
 
 // Add Snake Head
 function addSnakeHead() {
-    squares[currentIndexArr[currentIndexArr.length - 1]].classList.add('snake');
+    if(currentIndexArr.length > 1) {
+        squares[currentIndexArr[currentIndexArr.length - 1]].classList.add('snake', `snake-head-${currentSnakeHeadDirection}`);
+    } else {
+        squares[currentIndexArr[currentIndexArr.length - 1]].classList.add('snake');
+    }
+}
+
+// Remove Snake Head Style
+function removeSnakeHeadStyle() {
+    squares[currentIndexArr[currentIndexArr.length - 1]].classList.remove(...snakeHeadStyles);
 }
 
 // Change Snake Direction
@@ -52,21 +71,25 @@ function changeSnakeDirection(e) {
         case 'ArrowLeft':
             if(interval !== 1) {
                 interval = -1;
+                currentSnakeHeadDirection = 'left';
             }
             break;
         case 'ArrowRight':
             if(interval !== -1) {
                 interval = 1;
+                currentSnakeHeadDirection = 'right';
             }
             break;
         case 'ArrowUp':
             if(interval !== gridSize) {
                 interval = gridSize * -1;
+                currentSnakeHeadDirection = 'top';
             }
             break;
         case 'ArrowDown':
             if(interval !== gridSize * -1) {
                 interval = gridSize;
+                currentSnakeHeadDirection = 'bottom';
             }
             break;
     }
@@ -79,7 +102,9 @@ function moveSnake() {
     checkForTangle();
 
     // Remove Snake Tail
-    squares[currentIndexArr[0]].classList.remove('snake');
+    squares[currentIndexArr[0]].classList.remove('snake', ...snakeHeadStyles);
+
+    removeSnakeHeadStyle();
 
     let res = [];
 
